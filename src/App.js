6eebef754b5da616eb12f18acaf8159d96ced94b
Sharl Sherif <s.sherif@seamlabs.com>
2021-07-28 19:59:37 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [amount, setAmount] = useState(0);
+  const [years, setYears] = useState(30);
+  const [salary, setSalary] = useState(0);
+  const [result, setResult] = useState(null);
+
+  function calc(e, amount, years) {
+    e.preventDefault();
+    if (amount <= 0 || years > 30 || years <= 0 || salary <= 0) return;
+    let cash = (amount * 15) / 100;
+    amount -= (amount * 15) / 100;
+
+    let x = amount * (1.8 / 100);
+    let y = x * years;
+    const monthly = (y + amount) / years / 12;
+    setResult({
+      yearly: (y + amount) / years,
+      monthly,
+      cash,
+      isPossible: salary * 0.4 >= monthly,
+    });
+  }
+  const selectableYears = () => {
+    let arr = [];
+    for (let i = 1; i <= 30; i++) {
+      arr.push(i);
+    }
+    return arr;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row wrapper">
+        <h1 className="col-12 mb-3" dir="rtl">
+          حساب قيمة القسط الشهري لمبادره التمويل العقاري 3% لسنه 2021
+        </h1>
+        <hr />
+        <div class="col-12">
+          <form className=" justify-content-center mt-3">
+            <div class="form-group">
+              <label>مرتبك الشهري</label>
+              <input
+                type="number"
+                class="form-control"
+                placeholder="مثال:350000"
+                onInput={(e) => setSalary(e.target.value)}
+              />
+            </div>
+            <div class="form-group ">
+              <label>ثمن الشقة</label>
+              <input
+                type="number"
+                class="form-control"
+                placeholder="مثال:350000"
+                onInput={(e) => setAmount(e.target.value)}
+              />
+            </div>
+            <div class="form-group mt-2">
+              <label>عدد سنين القسط (حد اقصى 30 سنه)</label>
+              <select
+                class="form-control my-1 mr-sm-2"
+                onInput={(e) => setYears(e.target.value)}
+              >
+                {selectableYears().map((n) => (
+                  <option>{n}</option>
+                ))}
+              </select>
+            </div>
+            <button
+              type="submit"
+              class="btn mt-2 btn-primary"
+              onClick={(e) => calc(e, amount, years)}
+            >
+              احسب القسط
+            </button>
+          </form>
+          {result?.isPossible && (
+            <div class="col-12">
+              <table class="table  table-bordered  mt-3">
+                <thead>
+                  <tr>
+                    <th scope="col">هتدفع مقدم</th>
+                    <th scope="col">هتدفع شهريا</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">{Math.round(result.cash)} ج</th>
+                    <th scope="row">{Math.round(result.monthly)} ج</th>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          )}
+          {!result?.isPossible && result !== null && (
+            <div
+              class="alert alert-warning alert-dismissible fade show mt-3"
+              role="alert"
+            >
+              مينفعش تقسط الشقة دي لان القسط الشهري اكثر من 40% من مرتبك الحالي
+              ({salary} ج) لازم يكون القسط الشهري لا يتعدي ({salary * 0.4} ج)
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
